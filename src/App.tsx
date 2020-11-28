8f4eaa4f33c2591c304d43c1684489da13b48fea
Alexey Guskov <lexag@mail.ru>
2020-11-28 12:52:13 +0100

    feat: use ga-settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import { useSettings } from '@ombori/ga-settings';
 import logo from './logo.svg';
 import './App.css';
 
-declare global {
-  interface Window {
-    gridapp: any;
-  }
-}
-
-function initAppSettings(setAppSettings: Function) {
-  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    console.log('Settings development mode');
-    const settings = require('./settings.json');
-    settings.loaded = true;
-    console.log({ settings });
-    setAppSettings(settings as any);
-  } else {
-    console.log('Settings production mode');
-
-    window.addEventListener('GridappReady', () => {
-      // settings are ready for use!
-      const gridapp = window.gridapp;
-      const settings = gridapp.getSettings();
-      settings.loaded = true;
-      setAppSettings(settings as any);
-      console.log({ settings });
-    });
-  }
-}
+import { Schema as Settings } from './schema';
 
 function App() {
-  const [appSettings, setAppSettings] = useState({
-    loaded: false,
-    productName: '',
-    productPrice: '',
-  });
+  const settings = useSettings<Settings>();
 
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(() => {
-    if (appSettings.loaded) {
-      return;
-    }
-    initAppSettings(setAppSettings);
-  }, [appSettings.loaded]);
+  const productName = settings?.productName;
+  const productPrice = settings?.productPrice;
 
-  const { productName, productPrice } = appSettings;
+  if (!settings) {
+    return <div className="App">Loading gridapp settings...</div>
+  }
 
   return (
     <div className="App">
-      {appSettings.loaded ? (
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Product name: {productName}</p>
-          <p>Product price: {productPrice}</p>
-        </header>
-      ) : (
-        'Please wait, loading gridapp settings...'
-      )}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Product name: {productName}</p>
+        <p>Product price: {productPrice}</p>
+      </header>
     </div>
   );
 }
