460f7d210aadb8289bc0c040f4440ca46fd9e7d1
Alexey Guskov <alexey.guskov@ombori.com>
2022-12-16 15:53:32 +0100

    feat: analytics schema types

--- src/analytics-schema.ts
+++ src/analytics-schema.ts
@@ -1,5 +1,11 @@
+import { AnalyticsSchema } from '@ombori/ga-analytics-schema';
+
 /**
  * This file declares customized analytics reports that would be displayed for an application.
  */
 
-export default [];
+const schema: AnalyticsSchema = [{
+  title: "Test Report",
+}];
+
+export default schema;
